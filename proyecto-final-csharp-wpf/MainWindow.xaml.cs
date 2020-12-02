d75691d2077a1054508322d5453ae73c6db02003
Hugo Rodriguez <hi.rodriguez1998@gmail.com>
2020-12-02 17:56:19 -0300

    Agregado ABMC de Proveedor (Web API)

--- proyecto-final-csharp-wpf/MainWindow.xaml.cs
+++ proyecto-final-csharp-wpf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,133 @@ namespace proyecto_final_csharp_wpf
         {
             InitializeComponent();
         }
+
+        public async void ObtenerProveedores()
+        {
+            string result1 = await ProveedorAPI.ObtenerProveedores();
+            //string result1 = "[{\"idProveedor\":4,\"razonSocial\":\"Empresa A\",\"telefono\":\"021-369847\",\"direccion\":\"Calle A\",\"RUC\":\"123456789 -9\"},{\"idProveedor\":5, \"razonSocial\":\"Empresa B\",\"telefono\":\"068 -2688769\",\"direccion\":\"Calle B\",\"RUC\":\"45678912 -9\"},{\"idProveedor\":6,\"razonSocial\":\"Proveedor 1\",\"telefono\":\"0971 -396547\",\"direccion\":\"Direccion\",\"RUC\":\"454545456\"}]";
+
+            JArray JarrayProveedores = JArray.Parse(result1);
+
+
+
+            dg_proveedores.ItemsSource = JarrayProveedores;
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ObtenerProveedores();
+        }
+
+        private void Dg_proveedores_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (dg_proveedores.SelectedItem != null)
+                {
+                    dynamic proveedordynamic = dg_proveedores.SelectedItem;
+                    txt_ruc.Text = proveedordynamic.RUC;
+                    txt_razon.Text = proveedordynamic.razonSocial;
+                    txt_telefono.Text = proveedordynamic.telefono;
+                    txt_direccion.Text = proveedordynamic.direccion;
+                }
+            }
+            catch (Exception ex)
+            {
+                limpiar();
+            }
+            
+        }
+
+        private async void Btn_crear_Click(object sender, RoutedEventArgs e)
+        {
+            if (validarCampos() == true)
+            {
+                dynamic proveedordynamic = new ExpandoObject();
+                proveedordynamic.RUC = txt_ruc.Text;
+                proveedordynamic.razonSocial = txt_razon.Text;
+                proveedordynamic.telefono = txt_telefono.Text;
+                proveedordynamic.direccion = txt_direccion.Text;
+
+                var proveedoragregado = await ProveedorAPI.AgregarProveedor(proveedordynamic);
+                MessageBox.Show(proveedoragregado);
+                limpiar();
+                ObtenerProveedores();
+            }
+            
+
+        }
+
+        private async void Btn_modificar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dg_proveedores.SelectedItem != null)
+            {
+                if (validarCampos() == true)
+                {
+                    dynamic proveedordynamic = dg_proveedores.SelectedItem;
+
+                    proveedordynamic.RUC = txt_ruc.Text;
+                    proveedordynamic.razonSocial = txt_razon.Text;
+                    proveedordynamic.telefono = txt_telefono.Text;
+                    proveedordynamic.direccion = txt_direccion.Text;
+
+                    var proveedormodif = await ProveedorAPI.ModificarProveedor(proveedordynamic);
+                    MessageBox.Show(proveedormodif);
+                    limpiar();
+                    ObtenerProveedores();
+                }
+            }
+            else
+                MessageBox.Show("Debe seleccionar primeramente el cliente a modificar ");
+        }        
+
+        private async void Btn_eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dg_proveedores.SelectedItem != null)
+            {
+                dynamic proveedordynamic = dg_proveedores.SelectedItem;
+                var proveedoragregado = await ProveedorAPI.EliminarProveedor(proveedordynamic);
+                MessageBox.Show(proveedoragregado);
+                limpiar();
+                ObtenerProveedores();
+            }
+            else
+                MessageBox.Show("Debe seleccionar primeramente el cliente a eliminar ");
+        }
+
+        public void limpiar()
+        {
+            txt_ruc.Clear();
+            txt_razon.Clear();
+            txt_telefono.Clear();
+            txt_direccion.Clear();
+        }
+
+        public bool validarCampos()
+        {
+            if (txt_ruc.Text == "")
+            {
+                MessageBox.Show("El campo RUC no puede quedar en blanco");
+                return false;
+            }
+            if (txt_razon.Text == "")
+            {
+                MessageBox.Show("El campo Razón Social no puede quedar en blanco");
+                return false;
+            }
+            if (txt_telefono.Text == "")
+            {
+                MessageBox.Show("El campo Teléfono no puede quedar en blanco");
+                return false;
+            }
+            if (txt_direccion.Text == "")
+            {
+                MessageBox.Show("El campo Dirección no puede quedar en blanco");
+                return false;
+            }
+            return true;
+        }
     }
+    
 }
---
+++ proyecto-final-csharp-wpf/ProveedorAPI.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final_csharp_wpf
+{
+    class ProveedorAPI
+    {
+        public static async Task<string> ObtenerProveedores()
+        {
+            
+            using (var client = new HttpClient())
+            {
+               
+                client.BaseAddress = new Uri("https://proyectofinalasp-mvc2020.azurewebsites.net/api/");
+                
+                client.DefaultRequestHeaders.Accept.Clear();
+                
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));                
+                
+                HttpResponseMessage respuesta = await client.GetAsync("Proveedores");
+                
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    
+                    var proveedores = await respuesta.Content.ReadAsStringAsync();
+                    return proveedores;
+                }
+            }
+            return null;
+        }
+
+        public static async Task<string> AgregarProveedor(dynamic u)
+        {
+            
+            using (var client = new HttpClient())
+            {
+               
+                client.BaseAddress = new Uri("https://proyectofinalasp-mvc2020.azurewebsites.net/api/");
+
+                
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                StringContent content = new StringContent(JsonConvert.SerializeObject(u), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("Proveedores", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return "Se agrego satisfactoriamente";
+                }
+                else
+                {
+                    return "Ocurrio un error: " + response.StatusCode;
+                }
+            }
+        }
+
+      public static async Task<string> ModificarProveedor(dynamic u)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://proyectofinalasp-mvc2020.azurewebsites.net/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));               
+                StringContent content = new StringContent(JsonConvert.SerializeObject(u), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync("Proveedores/" + u.idProveedor, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return "Se modifico satisfactoriamente";
+                }
+                else
+                {
+                    return "Ocurrio un error: " + response.StatusCode;
+                }
+            }
+        }
+        public static async Task<string> EliminarProveedor(dynamic u)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://proyectofinalasp-mvc2020.azurewebsites.net/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));                
+                StringContent content = new StringContent(JsonConvert.SerializeObject(u), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.DeleteAsync("Proveedores/" + u.idProveedor);
+                if (response.IsSuccessStatusCode)
+                {
+                    return "Se modifico satisfactoriamente";
+                }
+                else
+                {
+                    return "Ocurrio un error: " + response.StatusCode;
+                }
+            }
+        }
+    }
+}
